ee84017316eec40df080e059128481959f4b8be7
Sarah Supp <sarah@weecology.org>
2019-07-17 13:57:29 -0400

    adds lines of code to be modified from FAL

--- code/create_effort_data_from_full.r
+++ code/create_effort_data_from_full.r
@@ -18,3 +18,26 @@ library(dggridR)
 
 # read in full eBird data to be converted into effort data
 effort = #FIXME, need effort file
+
+# FAL suggests using Fuller grid with resolution=6 (12452 km2)
+hex_6 <- dgconstruct(projection = "FULLER", aperture = 4, topology = "HEXAGON", res=6)
+
+dgearthgrid(hex_6, savegrid="cell_out-12.shp") # FIXME: Do I actually need this step or can I just pass the grid object directly onto the dataset, as with ISEA3H?
+
+
+# TODO: FAL other code, to modify for our needs here:
+library(rgdal)
+
+hex <- readOGR("cell_out-12.shp")
+hex@data$POLYFID <- 1:40962
+
+eBirdData$SUB_ID <- ifelse(!is.na(eBirdData$GROUP_ID), eBirdData$GROUP_ID, eBirdData$SUB_ID)
+eBirdData <- aggregate(cbind(LATITUDE, LONGITUDE) ~ PRIMARY_COM_NAME + YEAR + DAY + SUB_ID, data=eBirdData, FUN=mean, na.rm=TRUE)
+
+locs <- SpatialPoints(cbind(eBirdData$LONGITUDE, eBirdData$LATITUDE))
+proj4string(locs) <- proj4string(hex)
+ovr <- over(locs, hex)
+
+eBirdData$POLYFID <- ovr$POLYFID
+
+eft <- aggregate(SUB_ID ~ POLYFID + YEAR + DAY, data=eBirdData, FUN=length)
